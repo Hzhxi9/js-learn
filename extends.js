bd28009d1c28dcd672863b611d83095251acd971
Hzhxi9 <hzhixi9@gmail.com>
2021-09-02 11:34:37 +0800

    离线存储

--- extends.js
+++ extends.js
@@ -1,3 +1,74 @@
-/**对象的继承 */
+/**
+ * 编写一个 find(dataList, where) 方法
+ * 可以从下面数据结构中找到符合条件的数据
+ *   find(dataList, { id: 11111 });
+ *   -> { id: 11111, name: '坪洲', children: [] }
+ */
+const dataList = [
+  {
+    id: 1,
+    name: "广东省",
+    children: [
+      {
+        id: 11,
+        name: "深圳",
+        children: [
+          {
+            id: 111,
+            name: "宝安",
+            children: [
+              {
+                id: 1111,
+                name: "西乡",
+                children: [
+                  {
+                    id: 11111,
+                    name: "坪洲",
+                    children: [],
+                  },
+                  {
+                    id: 11112,
+                    name: "灵芝",
+                    children: [],
+                  },
+                ],
+              },
+              // ...
+            ],
+          },
+          // ...
+        ],
+      },
+      // ...
+    ],
+  },
+  {
+    id: 2,
+    name: "湖南省",
+    children: [
+      /* ... */
+    ],
+  },
 
-/**原型链继承 */
+  // ...
+];
+
+function find(dataList, id) {
+  return dataList.reduce((acc, cur) => {
+    return Object.assign(acc, cur.id === id ? cur : find(cur.children, id));
+  }, {});
+}
+console.log(find(dataList, 1111));
+
+const shape = {
+  radius: 10,
+  diameter() {
+    return this.radius * 2;
+  },
+  perimeter: () => 2 * Math.PI * this.radius,
+};
+
+// 下列语句的返回值分别是什么？
+console.log(((diameter) => diameter())());
+
+console.log(((s) => s.perimeter())(shape));
