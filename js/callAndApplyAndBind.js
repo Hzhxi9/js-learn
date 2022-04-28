cc125104cb2cf3d44fe64ebcb71d1e56cc6df6c4
huangzx <48216476+Hzhxi9@users.noreply.github.com>
2022-04-28 09:30:58 +0800

    feat: add call & apply & bind

--- js/callAndApplyAndBind.js
+++ js/callAndApplyAndBind.js
@@ -244,3 +244,196 @@ Function.prototype.call = function (ctx) {
   /**6. 返回值 */
   return result;
 };
+
+/**
+ * ===================================================================
+ * apply: 在使用 一个指定的 this 值和若干个指定的参数值的前提下调用某个函数或方法
+ * ===================================================================
+ */
+
+/**ES5 实现 */
+Function.prototype.apply = function (context, arr) {
+  /**1. 获取传入的对象, 当传入 null, 默认指向 window */
+  context = Object(context) || window;
+  /**2. 获取调用 apply 的函数, 设置为指向对象的属性 */
+  context.fn = this;
+  /**3. 定义一个变量接受返回值 */
+  var result;
+  /**4. 没有传入参数, 直接调用 */
+  if (!arr) result = context.fn();
+  else {
+    /**5. 存在参数, 将参数放入 args 数组 */
+    var args = [];
+    for (var i = 0, len = arr.length; i < len; i++) args.push(`arr[${i}]`);
+    /**6. 并传入到 context.fn 中 */
+    result = eval(`context.fn(${args})`);
+  }
+  /**7. 删除函数属性 */
+  delete context.fn;
+  /**8. 返回值 */
+  return result;
+};
+
+/**ES6 实现 */
+Function.prototype.apply = function (context) {
+  /**1. 获取传入的对象, 当传入 null, 默认指向 window */
+  context = context || window;
+  /**2. 获取调用 apply 的函数, 设置为指向对象的属性 */
+  context.fn = this;
+  /**3. 定义一个变量接受返回值 */
+  let result;
+  /**4. 没有传入参数, 直接调用 */
+  if (!arguments[1]) result = context.fn();
+  else {
+    /**5. 存在参数, 将参数放入 args 数组 */
+    const args = Array.prototype.slice.call(arguments, 1);
+    /**6. 并传入到 context.fn 中 */
+    result = context.fn(...args);
+  }
+  /**7. 删除函数属性 */
+  delete context.fn;
+  /**8. 返回值 */
+  return result;
+};
+
+/**
+ * ===================================================================================================
+ * bind: 当这个新函数被调用时, bind 的第一个参数作为它运行时的 this, 之后的一系列参数将会在传递实参前传入作为它的参数
+ * ===================================================================================================
+ */
+
+/**
+ * bind 的特点:
+ *  1. 返回一个函数
+ *  2. 可以传入参数
+ */
+
+/**返回函数的模拟实现 */
+var foo = { value: 1 };
+function bar() {
+  console.log(this.value);
+}
+// 返回一个函数
+var bindFoo = bar.bind(foo);
+bindFoo(); // 1
+
+/**第一版实现 */
+Function.prototype.bind = function (context) {
+  var _this = this;
+  return function () {
+    return _this.apply(context);
+  };
+};
+
+/**传参的模拟实现 */
+var foo = { value: 1 };
+function bar(name, age) {
+  console.log(this.value, name, age);
+}
+var bindFoo = bar.bind(foo, 'hello');
+bind(19); // 1 hello 19
+
+/**第二版实现: 可以在 bind 的时候传参, 在执行返回的函数的时候, 再传入一个参数 */
+Function.prototype.bind = function (context) {
+  var _this = this;
+  /**获取 bind 的时候第二个参数到最后一个参数 */
+  var args = Array.prototype.slice.call(arguments, 1);
+  return function () {
+    /**获取执行返回函数传入的参数  */
+    const _args = Array.prototype.slice.call(arguments);
+    return _this.apply(context, [...args, ..._args]);
+  };
+};
+
+/**
+ * 构造函数效果的模拟实现
+ *    一个绑定函数也能使用 new 操作符创建对象: 这种行为就像把原函数当作构造器
+ *    提供的 this 值被忽略, 同时调用时的参数被提供给模拟函数
+ **/
+var value = 2;
+var foo = { value: 1 };
+function bar(name, age) {
+  this.habit = 'shopping';
+  console.log(this.value, name, age);
+}
+
+bar.prototype.friend = 'tony';
+
+var BindFoo = bar.bind(foo, 'dist');
+
+var obj = new BindFoo(19); /**undefined tony 19 */
+
+console.log(obj.habit); /**shopping */
+console.log(obj.friend); /**tony */
+
+/**
+ * PS: 尽管在全局和 foo 中都声明了 value 值, 最后依然返回了 undefined,
+ *     说明 绑定的 this 失效, 此时的 this 已经指向了 obj
+ */
+
+/**第三版实现: 修改返回的函数的原型 */
+Function.prototype.bind = function (context) {
+  var _this = this,
+    args = Array.prototype.slice.call(arguments, 1);
+  var func = function () {
+    var _args = Array.prototype.slice.call(arguments);
+    /**
+     * 1. 当作为构造函数时, this 指向实例, 此时结果为 true, 将绑定函数的 this 指向该实例, 可以让实例获得来的绑定函数的值
+     *    以上面 demo 为例子, 如果改成 `this instanceof func? null: context`, 实例只是一个空对象, 将 null 改成 this ，实例会具有 habit 属性
+     * 2. 当作为普通函数时, this 指向 window, 此时结果为 false, 将绑定函数的 this 指向 context
+     */
+    return _this.apply(this instanceof func ? this : context, [...args, ..._args]);
+  };
+  /**修改返回函数的 prototype 为绑定函数的 prototype, 实例就可以继承绑定函数的原型中的值 */
+  func.prototype = this.prototype;
+  return func;
+};
+
+/**
+ *  `fBound.prototype = this.prototype`, 我们直接修改 fBound.prototype 的时候，也会直接修改绑定函数的 prototype
+ *  第四版实现: 通过一个空函数来进行中转
+ */
+Function.prototype.bind = function (context) {
+  var _this = this,
+    args = Array.prototype.slice.call(arguments, 1);
+
+  var FNO_P = function () {};
+
+  var func = function () {
+    var _args = Array.prototype.slice.call(arguments);
+    return _this.apply(this instanceof FNO_P ? this : context, [...args, ..._args]);
+  };
+  FNO_P.prototype = this.prototype;
+  func.prototype = new FNO_P();
+  return func;
+};
+
+/**最终版本 */
+Function.prototype.bind = function (context) {
+  /**1. 判断调用 bind 的是不是函数 */
+  if (typeof this !== 'function') throw new Error('Function.prototype.bind - what is trying to be bound is not callable');
+  /**2. 保存调用 bind 的函数 */
+  var _this = this;
+  /**3. 获取调用 bind 传入的参数 */
+  var args = Array.prototype.slice.call(arguments, 1);
+  /**4. 定义空函数进行中转 */
+  var NONE_FUNC = function () {};
+  /**5. 定义函数用于修改原型 */
+  var FUNC = function () {
+    /**6. 获取调用 bind 返回函数传入的参数 */
+    var bind_args = Array.prototype.slice.call(arguments);
+    /**
+     * 7. 返回函数
+     *    - 当返回的函数是构造函数时, 此时的 this 是指向实例, 即 new 出来的对象
+     *      将绑定函数的 this 指向该实例, 可以让实例获得来自绑定函数的值
+     *      以上面的是 demo 为例，如果改成 `this instanceof fBound ? null : context`，实例只是一个空对象，将 null 改成 this ，实例会具有 habit 属性
+     *    - 当作为普通函数时, this 指向 window, 此时将函数 this 指向 context
+     */
+    return _this.apply(this instanceof NONE_FUNC ? this : context, [...args, ..._args]);
+  };
+  /**8. 修改返回函数的 prototype 为绑定函数的 prototype, 实例就可以继承绑定函数的原型中的值 */
+  NONE_FUNC.prototype = this.prototype;
+  FUNC.prototype = new NONE_FUNC();
+  /**9. 返回函数 */
+  return FUNC;
+};
