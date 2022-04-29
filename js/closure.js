88c95c8f51a64f9b9f9497bc66816ffca4e62165
huangzx <48216476+Hzhxi9@users.noreply.github.com>
2022-04-29 11:02:50 +0800

    feat: add 闭包

--- js/closure.js
+++ js/closure.js
@@ -1,13 +1,170 @@
-var name = 'windows';
+/**闭包应用 */
 
-var ob = {
-    name: 'object',
-    getName: function(){
-        var that = this
-        return function(){
-            return that.name
-        }
-    }
+/**
+ * 1. 解决循环获取不到正确的索引 i
+ *    因为 div 节点的 onclick 事件是被异步触发的, 当事件被触发, 循环早已结束, 此时 i 的值已经为 5
+ **/
+try {
+  var nodes = document.querySelectorAll('.btn');
+  for (var i = 0, len = nodes.length; i < len; i++) {
+    /**
+     * 用闭包将每次循环的 i 封闭起来,
+     * 当在事件函数中顺着作用域链中从内到外查找变量 i 时,
+     * 会先找到被封闭在闭包环境中的 i */
+    (function (index) {
+      nodes[index].onclick = function () {
+        console.log(index);
+      };
+    })(i);
+  }
+} catch (error) {}
+
+/**
+ * 2. 封装变量: 可以帮助一些不需要暴露在全局的变量封装成私有变量
+ */
+
+var mult = (function () {
+  /**
+   * - 那些相同的参数存进缓存, 加入缓存机制提高这个函数的性能
+   * - 仅仅在 mult 函数中使用, 与其让 cache 变量跟 mult 函数一起平行暴露在全局作用域下, 不如把它封闭在函数内部里
+   *   - 减少页面的全局变量
+   *   - 避免这个变量在其他地方被不小心修改而引发错误
+   */
+  var cache = {};
+  /**
+   * 提炼函数:
+   *    将一个大函数内的能够独立出来一些代码块封装在独立出来的小函数里面
+   *        - 提高复用性
+   *        - 有良好的命名, 能起到注释的作用
+   *    如果这小小函数不需要在程序的其他地方使用, 最好将他们用闭包封装起来
+   * @returns {number}
+   */
+  function calc() {
+    var a = 1;
+    for (var i = 0, lens = arguments.length; i < lens; i++) a += arguments[i];
+    return a;
+  }
+  return function () {
+    var args = Array.prototype.join.call(arguments);
+    if (cache[args]) return cache[args];
+    return (cache[args] = calc.apply(null, arguments));
+  };
+})();
+
+console.log(mult(1, 2, 3, 4));
+console.log(mult(1, 2, 3, 4));
+
+/**
+ * 3. 延续局部变量的寿命
+ */
+var report = (function () {
+  var imgs = [];
+  return function (src) {
+    var img = new Image();
+    imgs.push(img);
+    img.src = src;
+  };
+})();
+
+/**
+ * 4. 闭包和面向对象设计: 对象以方法的形态包含过程, 而闭包则是在过程中以环境的形式包含了数据
+ */
+
+/**4.1 闭包保存变量 */
+var extent1 = (function () {
+  var value = 0;
+  return {
+    call() {
+      value++;
+      console.log(value);
+    },
+  };
+})();
+extent1.call(); // 1
+extent1.call(); // 2
+extent1.call(); // 3
+
+/**4.2 面向对象 */
+var extent2 = {
+  value: 0,
+  call: function () {
+    this.value++;
+    console.log(this.value);
+  },
+};
+extent2.call(); // 1
+extent2.call(); // 2
+extent2.call(); // 3
+
+/**4.3 构造函数 */
+function Extent() {
+  this.value = 0;
+  this.call = function () {
+    this.value++;
+    console.log(this.value);
+  };
 }
+var extent3 = new Extent();
+extent3.call(); // 1
+extent3.call(); // 2
+extent3.call(); // 3
 
-console.log(ob.getName()())
+/**
+ * 5. 用闭包实现命令模式
+ *
+ *   命令模式:
+ *      - 把请求封装为对象, 从而分离请求的发起者和请求的接收者(执行者)之间的耦合关系
+ *      - 在命令被执行之前, 可以预先往命令对象中植入命令的接收者
+ *
+ *   但在 JavaScript 中, 函数作为一等公民, 本身就可以四处传递, 用函数对象而不是普通对象来封装请求显得更加简单和自然
+ *   如果需要往函数对象中预先植入命令的接收者, 那么闭包可以完成这个工作
+ *
+ *   - 在面向对象版本的命令模式中, 预先植入的命令接收者被当成对象的属性保存起来
+ *   - 在闭包版本的命令模式中, 命令模式接收者会被封闭的闭包形成的环境中
+ */
+var Tv = {
+  open: function () {
+    console.log('open tv');
+  },
+  close: function () {
+    console.log('close tv');
+  },
+};
+
+/**5.1 面向对象 */
+var OpenTVCommand = function (receiver) {
+  this.receiver = receiver;
+};
+OpenTvCommand.prototype.execute = function () {
+  this.receiver.open(); // 执行命令, 打开电视机
+};
+
+OpenTvCommand.prototype.undo = function () {
+  this.receiver.close(); // 撤销命令, 关闭电视机
+};
+
+/**5.2 闭包 */
+var createCommand = function (receiver) {
+  var execute = function () {
+    return receiver.open(); // 执行命令, 打开电视机
+  };
+  var undo = function () {
+    return receiver.close(); // 撤销命令, 关闭电视机
+  };
+  return { execute, undo };
+};
+
+/**执行命令 */
+var setCommand = function (command) {
+  document.getElementById('execute').onclick = function () {
+    command.execute(); // 输出: 打开电视机
+  };
+  document.getElementById('undo').onclick = function () {
+    command.undo(); // 输出: 关闭电视机
+  };
+};
+/**面向对象 */
+setCommand(new OpenTvCommand(Tv));
+
+/**闭包 */
+setCommand(createCommand(Tv));
--- js/this.js
+++ js/this.js
@@ -1,84 +1,84 @@
 /**this 的指向 */
 
 /**1、作为对象的方法调用 */
-// var object_1 = {
-//   name: 'object_1',
-//   sayName() {
-//     console.log(this.name);
-//   },
-// };
+var object_1 = {
+  name: 'object_1',
+  sayName() {
+    console.log(this.name);
+  },
+};
 
-// /**指向直接调用函数的对象 */
-// object_1.sayName(); /**object_1 */
+/**指向直接调用函数的对象 */
+object_1.sayName(); /**object_1 */
 
-// /**2、作为普通函数调用 */
+/**2、作为普通函数调用 */
 
-// /**函数赋值给了变量, 此时this指向全局 */
-// const sayName_1 = object_1.sayName;
-// sayName_1(); /**undefined */
+/**函数赋值给了变量, 此时this指向全局 */
+const sayName_1 = object_1.sayName;
+sayName_1(); /**undefined */
 
-// global.name = 'global';
-// const sayName_2 = function () {
-//   return this.name;
-// };
-// /**此时this的指向也是全局 */
-// console.log(sayName_2()); /**global */
+global.name = 'global';
+const sayName_2 = function () {
+  return this.name;
+};
+/**此时this的指向也是全局 */
+console.log(sayName_2()); /**global */
 
-// global.name = 'outer_func';
-// const func_1 = function () {
-//   this.name = 'inner_func';
-//   var inner_func = function () {
-//     console.log(this.name); // inner_func
-//     console.log(this); // global
-//   };
-//   inner_func();
-// };
-// func_1();
+global.name = 'outer_func';
+const func_1 = function () {
+  this.name = 'inner_func';
+  var inner_func = function () {
+    console.log(this.name); // inner_func
+    console.log(this); // global
+  };
+  inner_func();
+};
+func_1();
 
-// /**3、构造器函数调用 */
+/**3、构造器函数调用 */
 
-// /**
-//  * 当用 new 运算符调用函数时, 该函数会返回一个对象
-//  * 通常情况下构造器中 this 会就会指向返回的这个对象
-//  */
-// function Object_1() {
-//   this.name = 'Object_1';
-// }
-// const o_1 = new Object_1();
-// /**3.1 隐式返回这个对象 */
-// console.log(o_1.name); // Object_1
+/**
+ * 当用 new 运算符调用函数时, 该函数会返回一个对象
+ * 通常情况下构造器中 this 会就会指向返回的这个对象
+ */
+function Object_1() {
+  this.name = 'Object_1';
+}
+const o_1 = new Object_1();
+/**3.1 隐式返回这个对象 */
+console.log(o_1.name); // Object_1
 
-// function Object_2() {
-//   this.name = 'Object_2';
-//   return { name: 'Object_2_1' };
-// }
-// const o_2 = new Object_2();
-// /**3.2 显式指向返回的对象 */
-// console.log(o_2.name); // Object_2_1
+function Object_2() {
+  this.name = 'Object_2';
+  return { name: 'Object_2_1' };
+}
+const o_2 = new Object_2();
+/**3.2 显式指向返回的对象 */
+console.log(o_2.name); // Object_2_1
 
-// function Object_3() {
-//   this.name = 'Object_3';
-//   return 'Object_3_1';
-// }
-// const o_3 = new Object_3();
-// /**3.3 不显式返回任何数据 or 返回一个非对象类型的数据 */
-// console.log(o_3.name); // Object_3
+function Object_3() {
+  this.name = 'Object_3';
+  return 'Object_3_1';
+}
+const o_3 = new Object_3();
+/**3.3 不显式返回任何数据 or 返回一个非对象类型的数据 */
+console.log(o_3.name); // Object_3
 
 /**
  * 4. Function.prototype.call & Function.prototype.apply
  **/
-// var Object_4 = {
-//   name: 'Object_4',
-//   sayName() {
-//     return this.name
-//   },
-// };
-// var Object_4_call = {
-//     name: 'Object_4_Call'
-// }
-// console.log(Object_4.sayName()) // Object_4
-// /**改变 this 的指向, 指向 Object_4_call 对象 */
-// console.log(Object_4.sayName.call(Object_4_call)) // Object_4_Call
+var Object_4 = {
+  name: 'Object_4',
+  sayName() {
+    return this.name
+  },
+};
+var Object_4_call = {
+    name: 'Object_4_Call'
+}
+console.log(Object_4.sayName()) // Object_4
+/**改变 this 的指向, 指向 Object_4_call 对象 */
+console.log(Object_4.sayName.call(Object_4_call)) // Object_4_Call
 
 /**
  * 概念:
@@ -95,55 +95,55 @@
  *    bind 后的函数无法在改 this 的指向
  */
 
-//  var name = 'window';
-//  var person = {
-//    name: 'person',
-//    sayName: function () {
-//      console.log(this.name);
-//    },
-//  };
-//  function sayName() {
-//    var sss = person.sayName;
-//    sss(); // window
-//    person.sayName(); // person
-//    (b = person.sayName)(); // window
-//  }
-//  sayName();
+ var name = 'window';
+ var person = {
+   name: 'person',
+   sayName: function () {
+     console.log(this.name);
+   },
+ };
+ function sayName() {
+   var sss = person.sayName;
+   sss(); // window
+   person.sayName(); // person
+   (b = person.sayName)(); // window
+ }
+ sayName();
 
-// var name = 'window';
-// var person1 = {
-//   name: 'person1',
-//   foo1: function () {
-//     console.log(this.name);
-//   },
-//   foo2: () => console.log(this.name),
-//   foo3: function () {
-//     return function () {
-//       console.log(this.name);
-//     };
-//   },
-//   foo4: function () {
-//     return () => {
-//       console.log(this.name);
-//     };
-//   },
-// };
+var name = 'window';
+var person1 = {
+  name: 'person1',
+  foo1: function () {
+    console.log(this.name);
+  },
+  foo2: () => console.log(this.name),
+  foo3: function () {
+    return function () {
+      console.log(this.name);
+    };
+  },
+  foo4: function () {
+    return () => {
+      console.log(this.name);
+    };
+  },
+};
 
-// var person2 = { name: 'person2' };
+var person2 = { name: 'person2' };
 
-// person1.foo1(); // person1
-// person1.foo1.call(person2); // person2
+person1.foo1(); // person1
+person1.foo1.call(person2); // person2
 
-// person1.foo2(); // window
-// person1.foo2.call(person2); // window
+person1.foo2(); // window
+person1.foo2.call(person2); // window
 
-// person1.foo3()(); // window
-// person1.foo3.call(person2)(); // window
-// person1.foo3().call(person2); // person2
+person1.foo3()(); // window
+person1.foo3.call(person2)(); // window
+person1.foo3().call(person2); // person2
 
-// person1.foo4()(); // person1
-// person1.foo4.call(person2)(); // person2
-// person1.foo4().call(person2); // person1
+person1.foo4()(); // person1
+person1.foo4.call(person2)(); // person2
+person1.foo4().call(person2); // person1
 
 var name = 'window';
 function Person(name) {
@@ -183,23 +183,32 @@ person1.foo4().call(person2); // person1
 var person3 = new (Person.bind(person1))('person3');
 person3.foo1();
 
-// let x = 3;
-// let obj = { x: 5 };
+/**
+ * =============================================
+ */
 
-// obj.fn = (function () {
-//   this.x *= ++x; /**this => window | x => 4 | this.x => undefined */
-//   return function (y) {
-//     this.x *= ++x + y;
-//     console.log(x, this.x)
-//   };
-// })();
+let x = 3;
+let obj = { x: 5 };
 
-// let fn = obj.fn;
-// obj.fn(6); /**this => obj | x => 5 | this.x => 5 * (5 + 6) => 55 */
-// fn(4); /**this => window | x => 6  */
-// console.log(obj.x, x); /**obj.x => 55 | x => 6 */
+obj.fn = (function () {
+  this.x *= ++x; /**this => window | x => 4 | this.x => undefined */
+  return function (y) {
+    this.x *= ++x + y;
+    console.log(x, this.x)
+  };
+})();
 
-var x = 3;
+let fn_1 = obj.fn;
+obj.fn(6); /**this => obj | x => 5 | this.x => 5 * (5 + 6) => 55 */
+fn(4); /**this => window | x => 6  */
+console.log(obj.x, x); /**obj.x => 55 | x => 6 */
+
+
+/**
+ * =============================================
+ */
+
+var x1 = 3;
 var obj = { x: 5 };
 
 obj.fn = (function () {
